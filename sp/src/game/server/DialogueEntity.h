ac36870f0d5b56422fe3b1e4f41060489ef6cf0e
DoctorZeppelin <breadjr6@gmail.com>
2022-12-22 23:27:41 -0600

    Dialogue Entity in server

---
+++ sp/src/game/server/DialogueEntity.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "cbase.h"
+class CDialogueEntity : public CLogicalEntity
+{
+public:
+	DECLARE_CLASS(CDialogueEntity, CLogicalEntity);
+	// Don't actually type this line!
+};
